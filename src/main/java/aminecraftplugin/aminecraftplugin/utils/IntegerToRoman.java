13c99faf04d4d22c7c28e37a009adc5e08b8c05c
pog <vermulstyfyerspeedruns@gmail.com>
2023-03-19 00:36:29 +0100

    commit

---
+++ src/main/java/aminecraftplugin/aminecraftplugin/utils/IntegerToRoman.java
@@ -0,0 +1,21 @@
+package aminecraftplugin.aminecraftplugin.utils;
+
+public class IntegerToRoman {
+
+    public static String integerToRoman(int num) {
+
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] romanLiterals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        StringBuilder roman = new StringBuilder();
+
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                num -= values[i];
+                roman.append(romanLiterals[i]);
+            }
+        }
+        return roman.toString();
+    }
+
+}

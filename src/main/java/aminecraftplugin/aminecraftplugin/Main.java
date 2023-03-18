6246d499378ef37a3a4f432930dbbce67b249043
pog <vermulstyfyerspeedruns@gmail.com>
2023-03-19 00:48:14 +0100

    commit

--- src/main/java/aminecraftplugin/aminecraftplugin/Main.java
+++ src/main/java/aminecraftplugin/aminecraftplugin/Main.java
@@ -3,15 +3,24 @@ package aminecraftplugin.aminecraftplugin;
 import aminecraftplugin.aminecraftplugin.drill.Resource;
 import aminecraftplugin.aminecraftplugin.market.Market;
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public final class Main extends JavaPlugin {
 
+    private static String path;
+
     @Override
     public void onEnable() {
+
+        //file path
+        path = getDataFolder().getAbsoluteFile().toString();
+
         // Plugin startup logic. test
 
         ArrayList<Listener> events = new ArrayList<>();
@@ -32,5 +41,10 @@ public final class Main extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+
+    public static void saveFile(YamlConfiguration file, String s) throws IOException {
+        file.save(new File(path,s));
     }
 }
--- src/main/java/aminecraftplugin/aminecraftplugin/drill/Resource.java
+++ src/main/java/aminecraftplugin/aminecraftplugin/drill/Resource.java
@@ -1,17 +1,24 @@
 package aminecraftplugin.aminecraftplugin.drill;
 
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.inventory.ItemStack;
 
+import java.io.File;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class Resource {
 
 
     public static HashMap<Integer, Resource> resources;
 
+
+    YamlConfiguration resourceFile;
+
     private ItemStack itemStack;
     private String name;
-    private Float value;
+    private Double value;
 
 
     public static void init(){
@@ -25,9 +32,33 @@ public class Resource {
 
     public void saveResources(){
 
+        for (Map.Entry<Integer, Resource> set : resources.entrySet()) {
+
+            int id = set.getKey();
+            Resource resource = set.getValue();
+            resourceFile.set(id + ".itemstack", resource.getItemStack());
+            resourceFile.set(id + ".name", resource.getName());
+            resourceFile.set(id + ".value", resource.getValue());
+
+        }
+        //resourceFile.save(new File(), "");
+
     }
 
     public static HashMap<Integer, Resource> loadResources(){
         return new HashMap<>();
+    }
+
+
+    public ItemStack getItemStack() {
+        return itemStack;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getValue() {
+        return value;
     }
 }

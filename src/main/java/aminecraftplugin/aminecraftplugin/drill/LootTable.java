b603981f4f7ae82203f789e205a14b780417089e
pog <vermulstyfyerspeedruns@gmail.com>
2023-03-21 17:02:34 +0100

    bug fixes

--- src/main/java/aminecraftplugin/aminecraftplugin/drill/LootTable.java
+++ src/main/java/aminecraftplugin/aminecraftplugin/drill/LootTable.java
@@ -1,9 +1,16 @@
 package aminecraftplugin.aminecraftplugin.drill;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.HashMap;
+
+import static aminecraftplugin.aminecraftplugin.utils.ChatUtils.format;
 
 public class LootTable {
 
@@ -43,7 +50,26 @@ public class LootTable {
     }
 
     public static void openSelectLoottableMenu(Player p){
+        Inventory selectInventory = Bukkit.createInventory(null, 54, "Select Loot Table");
 
+
+        ItemStack leftArrow = new ItemStack(Material.ARROW);
+        ItemMeta meta = leftArrow.getItemMeta();
+        meta.setDisplayName("Previous page");
+        leftArrow.setItemMeta(meta);
+
+        ItemStack rightArrow = new ItemStack(Material.ARROW);
+        ItemMeta meta2 = rightArrow.getItemMeta();
+        meta2.setDisplayName("Next page");
+        rightArrow.setItemMeta(meta2);
+
+        ItemStack grayGlass = new ItemStack(Material.GRAY_STAINED_GLASS_PANE);
+        ItemMeta meta3 = grayGlass.getItemMeta();
+        meta3.setDisplayName(format("&7"));
+        grayGlass.setItemMeta(meta3);
+
+
+        selectInventory.setItem(45, leftArrow);
     }
 
 

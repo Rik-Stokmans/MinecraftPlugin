a7b7973c2a17822a4b99a77c2f1bebdfc04c34d4
Rik-Stokmans <71502476+Rik-Stokmans@users.noreply.github.com>
2023-03-31 16:01:43 +0200

    updated Market.java

--- src/main/java/aminecraftplugin/aminecraftplugin/market/Market.java
+++ src/main/java/aminecraftplugin/aminecraftplugin/market/Market.java
@@ -433,6 +433,7 @@ public class Market implements Listener {
                             + (worth * (strength * Math.log(Math.abs(strength))) - (worth * (2 * x1 + strength * Math.log(Math.abs(x1 - strength)))));
                 }
 
+                playerProfile.getBackPack().addResource(key, amountSold * -1);
                 stock += amountSold;
                 market.trades.get(key).setStock(stock);
                 market.trades.get(key).tick(false);

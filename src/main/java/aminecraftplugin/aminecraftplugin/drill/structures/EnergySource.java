dee008e59b5f00dd9a48a027abac528bc391e4f5
pog <vermulstyfyerspeedruns@gmail.com>
2023-04-03 23:48:17 +0200

    energy source

--- src/main/java/aminecraftplugin/aminecraftplugin/drill/structures/EnergySource.java
+++ src/main/java/aminecraftplugin/aminecraftplugin/drill/structures/EnergySource.java
@@ -1,18 +1,30 @@
 package aminecraftplugin.aminecraftplugin.drill.structures;
 
+import aminecraftplugin.aminecraftplugin.drill.loot.LootFinder;
+import me.filoghost.holographicdisplays.api.hologram.Hologram;
 import org.bukkit.Location;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.block.BlockState;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.UUID;
 
 public class EnergySource implements Structure, Listener {
 
+    private OfflinePlayer owner;
+    private Location location;
+    private ArrayList<Inventory> pages = new ArrayList<>();
+    private Hologram hologram;
+    private org.bukkit.structure.Structure structure;
+    private ArrayList<Integer> tasks = new ArrayList<>();
 
     @Override
     public void place(Player p) {
